cd3588d6f77ca6cb3c1a6780711c2129e26d71f0
Sarvar Khalimov <khalimovsarvar@gmail.com>
2023-02-14 21:41:09 +0500

    Solve the lesson #3

--- lesson3/src/main/resources/SOLUTION.sql
+++ lesson3/src/main/resources/SOLUTION.sql
@@ -1,2 +1,14 @@
 
-ALTER TABLE student MODIFY COLUMN birthday DATE NOT NULL;
+-- ALTER TABLE student MODIFY COLUMN birthday DATE NOT NULL;
+ALTER TABLE student ALTER COLUMN birthday SET NOT NULL;
+
+ALTER TABLE mark ADD CHECK (makr >= 1 AND mark <= 10);
+ALTER TABLE mark ALTER COLUMN subject_id SET NOT NULL;
+
+ALTER TABLE subject ADD CHECK (grade >= 1 AND grade <= 5);
+
+ALTER TABLE paymenttype ADD UNIQUE (name);
+
+ALTER TABLE payment ALTER COLUMN type_id SET NOT NULL;
+ALTER TABLE payment ALTER COLUMN amount SET NOT NULL;
+ALTER TABLE payment ALTER COLUMN date SET NOT NULL;
