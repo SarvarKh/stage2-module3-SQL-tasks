6845cbcf049912350c328b89b9dcfe4d67185c56
Sarvar Khalimov <khalimovsarvar@gmail.com>
2023-02-15 16:12:23 +0500

    Solve fully the lesson #6

--- lesson6/src/main/resources/SOLUTION.sql
+++ lesson6/src/main/resources/SOLUTION.sql
@@ -4,4 +4,7 @@ SELECT * FROM payment JOIN paymenttype ON payment.type_id = paymenttype.id WHERE
 SELECT * FROM mark JOIN subject on mark.subject_id = subject.id WHERE subject.name = "Art";
 
 # Select all students who have “WEEKLY” payments;
+SELECT DISTINCT s.id, s.name, s.birthday, s.groupnumber FROM payment AS p JOIN paymenttype AS t ON p.type_id = t.id JOIN student AS s ON p.student_id = s.id WHERE t.name = 'WEEKLY';
+
 # Select all students who has marks by Math.
+SELECT * FROM mark AS m JOIN subject AS s ON m.subject_id = s.id JOIN student AS st ON m.student_id = st.id WHERE s.name = 'Math';
